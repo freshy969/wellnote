091e42d15ac9cc844b2d83c8d9c27e27be658770
Henshal <henshal@Henshals-MacBook-Air.local>
2024-02-24 08:49:31 +0530

    icons: Added brand icons

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -7,12 +7,16 @@ import {
   Card,
   Flex,
   ScrollArea,
+  TextInput,
+  Stack,
 } from "@mantine/core";
 import {
   IconPhoto,
   IconMessageCircle,
+  IconUpload,
+  IconSend2,
 } from "@tabler/icons-react";
-import React from "react";
+import React, { useRef } from "react";
 import { useEffect, useState } from "react";
 import { readNotes } from "../query/notes";
 import { random } from "../utils/generic/helper";
@@ -81,9 +85,31 @@ export default function Home({ user }: any) {
   const noteContent = <>{lazyNote ? lazyNote : null}</>;
   const passwordContent = <>{lazyPassword ? lazyPassword : null}</>;
 
+  const [message, setMessage] = useState([]);
+  const [q, setQ] = useState(false);
+  const messagesRef = useRef(null);
+
+  const handleKeyPress = (event: any) => {
+    if (event.key === "Enter") {
+      setMessage([...message, event.target.value]);
+      setQ(true);
+    }
+  };
+
+  const scrollMessagesToBottom = () => {
+    if (messagesRef.current) {
+      messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
+    }
+  };
+
+  useEffect(() => {
+    // Scroll to the bottom when component mounts or when message array changes
+    scrollMessagesToBottom();
+  }, [message]);
+
   return (
     <>
-      <Tabs mt={"lg"} variant={"pills"} radius="md" defaultValue="passwords">
+      <Tabs mt={"lg"} variant={"pills"} radius="md" defaultValue="upload">
         <Tabs.List>
           <div
             style={{
@@ -95,6 +121,13 @@ export default function Home({ user }: any) {
             }}
           >
             <Tabs.Tab
+              value="upload"
+              leftSection={<IconUpload style={iconStyle} />}
+            >
+              Smart Upload
+            </Tabs.Tab>
+            <Tabs.Tab
+              ml={"xs"}
               value="passwords"
               leftSection={<IconPhoto style={iconStyle} />}
             >
@@ -109,6 +142,61 @@ export default function Home({ user }: any) {
             </Tabs.Tab>
           </div>
         </Tabs.List>
+
+        <Tabs.Panel h={"65vh"} mt={"sm"} value="upload">
+          <Stack
+            gap={"xs"}
+            h={"100%"}
+            bg="var(--mantine-color-body)"
+            justify="flex-end"
+          >
+            <div
+              ref={messagesRef}
+              style={{ maxHeight: "65vh", overflowY: "auto" }}
+            >
+              {q
+                ? message.map((e, index) => (
+                    <>
+                      <Flex key={index} mt={"xs"} justify={"end"}>
+                        <Button
+                          px={"sm"}
+                          size={"md"}
+                          style={{
+                            padding: rem(5),
+                            borderRadius: "20px 20px 0px 20px",
+                          }}
+                        >
+                          <Text>{e}</Text>
+                        </Button>
+                      </Flex>
+
+                      <Flex key={index} mt={"xs"} justify={"start"}>
+                        <Button
+                          px={"sm"}
+                          size={"md"}
+                          variant={"default"}
+                          style={{
+                            padding: rem(5),
+                            borderRadius: "20px 20px 20px 0px",
+                          }}
+                        >
+                          <Text>Uploading</Text>
+                        </Button>
+                      </Flex>
+                    </>
+                  ))
+                : null}
+            </div>
+
+            <TextInput
+              radius={"md"}
+              onKeyDown={(e) => handleKeyPress(e)}
+              rightSection={<IconSend2 onClick={(e) => handleKeyPress(e)} />}
+              size={"lg"}
+              autoFocus
+            />
+          </Stack>
+        </Tabs.Panel>
 
         <Tabs.Panel mt={"sm"} value="passwords">
           <Flex justify={"space-between"} align={"center"}>
